543ab3fab93ae6933d5e1f5921e3503f2e49554a
Mohamed Abdelhay <moabdelhay@banquemisr.com>
2023-03-06 17:51:24 +0200

    TicketNum4 :: Adding Exception Handlers

---
+++ src/main/java/com/drones/api/exception/MedicationInvalidDataException.java
@@ -0,0 +1,6 @@
+package com.drones.api.exception;
+
+public class MedicationInvalidDataException extends RuntimeException {
+
+    public MedicationInvalidDataException(String message) {super(message);}
+}
